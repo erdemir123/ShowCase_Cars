ab415c037b02f03c2e2a107fbd6990065518e1d4
erdemir123 <sadikerdemir93@gmail.com>
2023-07-11 19:24:11 +0300

    navbar completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,28 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Car Hub',
-  description: 'Discover the best cars in thr world ',
-}
+  title: "Car Hub",
+  description: "Discover the best cars in thr world ",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className="relative">{children}</body>
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
