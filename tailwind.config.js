b29d6c9be7b01947811c4fc29a043b4a1d082c11
erdemir123 <sadikerdemir93@gmail.com>
2023-07-11 19:37:29 +0300

    footer completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'pattern': "url('/pattern.png')",
-        'hero-bg': "url('/hero-bg.png')"
+        "pattern": "url('/pattern.png')",
+        "hero-bg": "url('/hero-bg.png')",
       },
-      colors:{
-        "primary-blue":{
+      colors: {
+        "primary-blue": {
           DEFAULT: "#2B59FF",
           100: "#F5F8FF",
-        }
-      }
+        },
+        "black-100": "#2B2C35",
+      },
     },
   },
   plugins: [],
-}
+};
